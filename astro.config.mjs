f14a98e5371e9f1a23c9def12b0d1c871c61c879
Kotaro Ozawa <162984306+kotaro9967@users.noreply.github.com>
2026-09-03 01:08:06 +0900

    Add GitHub Pages build mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kotaro-ozawa-portfolio.netlify.app',
+  site: isGitHubPages
+    ? 'https://kotaro9967.github.io'
+    : 'https://kotaro-ozawa-portfolio.netlify.app',
+  base: isGitHubPages ? '/My-Portfolio-temporary-' : '/',
   // GitHub Pages のサブディレクトリに置く場合は base: '/repo-name' を指定
   integrations: [sitemap()],
   build: {
